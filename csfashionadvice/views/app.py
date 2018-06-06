74be1ef81a26760d24e078dd0ba17db6ad7db5ca
Elise Shanholtz <elise.shanholtz@capitalone.com>
2018-06-06 17:42:12 -0400

    fixed file variable

--- csfashionadvice/views/app.py
+++ csfashionadvice/views/app.py
@@ -5,8 +5,6 @@ CS Fashion Advice index view.
 import os
 
 import flask
-from flask import request
-
 import csfashionadvice
 
 UPLOAD_FOLDER = os.path.basename('uploads')
@@ -20,15 +18,9 @@ def show_index():
 
 @csfashionadvice.app.route('/score', methods=['POST'])
 def score_image():
-    file = request.files['image']
-<<<<<<< HEAD
-    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-=======
+    file = flask.request.files['image']
     f = os.path.join(flask.app.config['UPLOAD_FOLDER'], file.filename)
->>>>>>> 57b83140137ec45193811e2d7b3553214cb86b06
-
     file.save(f)
-    score = 0
 
     context = {'filename': f, 'score': 0}
     return flask.render_template("score.html", **context)
