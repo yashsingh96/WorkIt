a35dcbe6473dfadc7c8ff58543038347258f48a6
pxd338 <michael.peretz@capitalone.com>
2018-06-07 09:27:22 -0400

    okurrr mamwa

--- csfashionadvice/csfashionadvice.py
+++ csfashionadvice/csfashionadvice.py
@@ -2,17 +2,34 @@
 CS Fashion Advice index view.
 """
 import flask
-from flask import Flask
+from flask import Flask, flash, session
 import os
+
+from flask_session import Session
 
 app = Flask(__name__)
 UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+sess = Session()
+
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+sess.init_app(app)
+
+app.debug = True
 
 
 @app.route('/')
 def show_index():
     return flask.render_template("index.html")
+
+
+@app.route('/feedback')
+def show_feedback_box():
+    message = "Thanks for the feedback!"
+    flash(message)
+    return flask.render_template("test.html")
 
 
 @app.route('/score', methods=['POST'])
