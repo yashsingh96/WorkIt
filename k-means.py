959aaa8fc88300a08a717ed4be607000b990e8f4
Austin Bae <hyunsuk.bae@capitalone.com>
2018-06-07 09:32:01 -0400

    updated k-means to support pruning and ranking

--- k-means.py
+++ k-means.py
@@ -4,6 +4,30 @@ from sklearn.cluster import KMeans
 import numpy as np
 from sys import argv
 import argparse
+import collections
+
+def prune(labels, lst, threshold):
+    for index in range(len(lst)-1):
+        for i in range(index+1, len(lst)):
+            if lst[index][0]>0 and lst[i][0]>0 and abs(lst[index][0]-lst[i][0]) < threshold and abs(lst[index][1]-lst[i][1]) < threshold and abs(lst[index][2]-lst[i][2]) < threshold:
+                lst[i] = (-1,-1,index)
+    for i in range(len(lst)):
+        if lst[i][0] < 0:
+            labels[labels==i] = lst[i][2]
+    idx = 0
+    print(lst)
+
+    while idx < len(lst):
+        if lst[idx][0] < 0:
+            lst = np.delete(lst, idx, axis=0)
+            for i in range(idx, len(lst)):
+                    labels[labels==i+1] = i
+        else:
+            idx += 1
+    print(labels[labels>=len(lst)])
+    print(lst)
+    return lst, labels
+
 
 img = cv2.imread(argv[1])
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -12,37 +36,68 @@ plt.figure()
 plt.axis("off")
 #plt.imshow(img),plt.colorbar(),plt.show()
 
-clusters = 10
+clusters = 8
 image = img.reshape((img.shape[0] * img.shape[1], 3))
 clt = KMeans(n_clusters = clusters)
 clt.fit(image)
 
-print(image)
-print(clt.cluster_centers_)
-print(clt.labels_)
+labels = np.copy(clt.labels_)
+centers = np.copy(clt.cluster_centers_)
 
-classification = clt.labels_.reshape(img.shape[:2])
+centers, labels = prune(labels, centers, 40)
+
+clusters = len(centers)
+
+classification = labels.reshape(img.shape[:2])
 #getting shirt color
 
 x_range = list(range(int(img.shape[1]*0.4), int(img.shape[1]*0.6)))
 y_top = list(range(int(img.shape[0]*0.5)))
 y_bottom = list(range(int(img.shape[0]*0.5), int(img.shape[0])))
 
-print(img.shape)
 
-print(classification.shape)
-shirt_count = np.bincount(classification[:int(img.shape[0]*0.6), int(img.shape[1]*0.33):int(img.shape[1]*0.66)].flatten())
-pants_count = np.bincount(classification[int(img.shape[0]*0.7):,int(img.shape[1]*0.33):int(img.shape[1]*0.66)].flatten())
+top_count = np.bincount(classification[:int(img.shape[0]*0.5), int(img.shape[1]*0.25):int(img.shape[1]*0.75)].flatten())
+bottom_count = np.bincount(classification[int(img.shape[0]*0.5):,int(img.shape[1]*0.25):int(img.shape[1]*0.75)].flatten())
 
-bar = np.zeros((50, 100, 3), dtype='uint8')
+#find background color
+back_x = [0, 1, img.shape[1]-2, img.shape[1]-1]
+back_y = [0, 1, img.shape[0]-2, img.shape[0]-1]
+count_back = [0 for x in range(clusters)]
+for i in back_x:
+    for j in back_y:
+        count_back[classification[j,i]] += 1
 
-shirt_color = clt.cluster_centers_[np.argmax(shirt_count)]
-pants_color = clt.cluster_centers_[np.argmax(pants_count)]
+background_color = np.argmax(count_back)
+top_count[background_color] = 0
+bottom_count[background_color] = 0
 
-print(shirt_color)
-print(pants_color)
+bar = np.zeros((100, 200, 3), dtype='uint8')
 
-bar[:,:50] = shirt_color
-bar[:,50:] = pants_color
+total_top = np.sum(top_count)
+total_bottom = np.sum(bottom_count)
+top_curr = 0
+bot_curr = 0
 
-plt.imshow(bar),plt.colorbar(),plt.show()
+top_rank = []
+bottom_rank = []
+
+for i in range(clusters-1):
+    max_top = np.argmax(top_count)
+    max_bot = np.argmax(bottom_count)
+    top_next = top_curr+int(top_count[max_top]/total_top*100)
+    bot_next = bot_curr+int(bottom_count[max_bot]/total_bottom*100)
+    top_rank.append((centers[max_top], int(top_count[max_top]/total_top*100)))
+    bottom_rank.append((centers[max_bot], int(bottom_count[max_bot]/total_bottom*100)))
+
+    if top_next < 100 and bot_next < 100:
+        bar[top_curr:top_next,:100] = centers[max_top]
+        bar[bot_curr:bot_next,100:] = centers[max_bot]
+
+    top_curr = top_next
+    bot_curr = bot_next
+    top_count[max_top] = 0
+    bottom_count[max_bot] = 0
+
+print(top_rank)
+print(bottom_rank)
+#plt.imshow(bar),plt.colorbar(),plt.show()
