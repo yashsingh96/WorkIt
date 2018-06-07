553d583db0b97e214febd078e309ef1527349f0e
pxd338 <michael.peretz@capitalone.com>
2018-06-07 10:21:30 -0400

    okurrr mamwa

--- classifier.py
+++ classifier.py
@@ -1,5 +1,8 @@
+from colorsys import rgb_to_hsv
+
 from appropriate_check import too_revealing
-from colour_match import get_colours, matches
+from colour_match import matches
+from kmeans import get_colours
 
 
 def fit(image):
@@ -7,18 +10,37 @@ def fit(image):
 
 
 # Scores the outfit based on style
-def get_style_score(image, gender):
+def get_style_score(image, gender, plot = True):
     score = 0
 
-    colours = get_colours(image, False)
+    top, bottom = get_colours(image)
+
+    print(bottom)
+    print(top)
+
+    top = top[0][0]
+    # bottom = bottom[0]
+
+    top = rgb_to_hsv(top[0] / 255, top[1] / 255, top[2] / 255)
+    top = (top[0] * 359, top[1], top[2])
+
+    bottom = bottom[0][0]
+    # bottom = bottom[0]
+
+    bottom = rgb_to_hsv(bottom[0] / 255, bottom[1] / 255, bottom[2] / 255)
+    bottom = (bottom[0] * 359, bottom[1], bottom[2])
 
     # If it doesn't match a colour scheme, it's ugly
-    if not matches(colours):
+    if not plot or not matches((top, bottom)):
         score -= 1
 
     # if untucked long button shirt, it's ugly
-    if gender == "M" and not fit(image):
-        score -= 1
+    # if gender == "M" and not fit(image):
+    #     score -= 1
+
+    print(score)
+
+    return score
 
 
 def button_shirt(image, gender):
@@ -43,3 +65,11 @@ def get_appropriate_score(image, gender):
             score -= 1
         elif capital_one_logo(image):
             score += 1
+
+
+def main():
+    get_style_score("images/bad/nomatch.jpg", "M", False)
+
+
+if __name__ == "__main__":
+    main()
